ed8dbddc77b05d00c92ac97b3ad152fa3d05275b
Giorgio <miticojo@users.noreply.github.com>
2015-11-27 09:37:58 +0100

    Update setup.py
    fixed typo name

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(
-    name='speedtest-clu',
+    name='speedtest-cli',
     version='1.0',
     packages=[''],
-    url='',
+    url='https://github.com/miticojo/speedtest-cli',
     license='MIT',
     author='Giorgio Crivellari',
     author_email='miticojo[at]gmail.com',
